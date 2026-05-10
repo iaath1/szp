0488a2a8bff8471e3b908c96ecde8bdb099ac1ca
iaath1 <mykhailostozhkov@gmail.com>
2026-05-10 14:24:00 +0200

    first commit

---
+++ src/main/java/com/stg/szp/SzpApplication.java
@@ -0,0 +1,13 @@
+package com.stg.szp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stg/szp/SzpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stg.szp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
